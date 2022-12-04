2eef54486c8c06e9c3365359f70fb3a2066a2a2f
Leyla Shafiyeva <llshafi03@gmail.com>
2022-12-05 00:40:06 +0400

    add CoR

---
+++ CoR/Program.cs
@@ -0,0 +1,57 @@
+﻿abstract class Translator
+{
+    protected Translator? Next { get; set; }
+
+    public virtual Translator SetNext(Translator next)
+    {
+        Next = next;
+        return this;
+    }
+
+    public abstract void Translate();
+}
+
+
+class EngToGerman : Translator
+{
+    public override void Translate()
+    {
+        Console.WriteLine("Translated from English to German");
+        Next?.Translate();
+    }
+}
+
+class EngToChn : Translator
+{
+    public override void Translate()
+    {
+        Console.WriteLine("Translated from English to Chinese");
+        Next?.Translate();
+    }
+}
+
+class EngToRus : Translator
+{
+    public override void Translate()
+    {
+        Console.WriteLine("Translated from English to Russian");
+        Next?.Translate();
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Translator translator = new EngToGerman();
+        translator.Translate();
+
+        Console.WriteLine();
+        translator = new EngToChn()
+            .SetNext(new EngToRus());
+        translator.Translate();
+
+
+    }
+}
